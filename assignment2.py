2d14fe72e5e0058444f1c7701672b966f06b8465
MaximeCamille <mcami013@fiu.edu>
2021-01-31 15:18:47 -0500

    Added 'else'statement for task 4

--- assignment2.py
+++ assignment2.py
@@ -37,6 +37,8 @@ class Assignment2:
                 newString += name[i - 1].lower()
             elif (i % 5 == 0) and (i % 4 != 0) and (i % 3 != 0):
                 newString += " "
+            else:
+                newString += name[i - 1]
         
         return newString
 
